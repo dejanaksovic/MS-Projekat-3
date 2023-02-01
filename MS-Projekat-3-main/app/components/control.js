41fc11dfe05257b7434db3cf493b77bdf161375e
Marko Popovic <104674760+popovicmarko@users.noreply.github.com>
2023-02-01 13:47:04 +0100

    Started html and css

---
+++ MS-Projekat-3-main/app/components/control.js
@@ -0,0 +1,3 @@
+export default function control() {
+    this.body = '<div class="control"> Control </div>'
+} 
---
+++ MS-Projekat-3-main/app/components/product.js
@@ -0,0 +1,14 @@
+export default function Product() {
+    this.body = `<div class='product'>Product ${this.value}</div>`
+    this.value = "00000000"
+    this.set = (value) => {
+        this.value = value
+        while(this.value.length < 16) {
+            this.value = 0 + this.value
+        }
+    }
+    this.render = () => {
+        this.body = `<div class='product'>Product ${this.value}</div>`
+        return this.body
+    }
+}
