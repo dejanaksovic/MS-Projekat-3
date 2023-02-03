1e4aeafa9dd4919e3ef99bff805337762bdac2ce
Dejan Aksovic <imdejan00@gmail.com>
2023-02-03 20:24:18 +0100

    Finished most of the validation

--- MS-Projekat-3-main/app/app.js
+++ MS-Projekat-3-main/app/app.js
@@ -5,9 +5,14 @@ import Control from './components/control.js'
 import ALU from './components/ALU.js'
 import formater from './middleware/formatMiddleware.js'
 
+//app
 const app = document.querySelector("#main")
-const button = document.querySelector("button")
 
+//buttons
+const buttonTest = document.querySelector(".test")
+const buttonSubmit = document.querySelector(".form-button")
+
+//Components
 const mulLocal = new Multiplicand()
 const mulpLocal = new Multiplier()
 const product = new Product(8)
@@ -15,13 +20,13 @@ const controlLocal = new Control()
 const formaterLocal = new formater()
 const ALULocal = new ALU()
 
-
-
+//Connecting components
 ALULocal.steps.push(mulpLocal.shiftR)
 ALULocal.steps.push(formaterLocal.addBin)
 ALULocal.steps.push(mulLocal.shiftL)
 
-button.addEventListener("click", e=> {
+//Multiplier logic
+buttonTest.addEventListener("click", e=> {
     const test = ALULocal.stepFunc(product.value, mulLocal.value, mulpLocal.value.slice(-1))
     
     if(test) {console.log(test)
@@ -31,3 +36,87 @@ button.addEventListener("click", e=> {
 })
 
 app.innerHTML = mulLocal.render() + mulpLocal.render() + product.render()+ ALULocal.render() + controlLocal.body
+
+//Kreiranje aplikacije
+app.style.setProperty("display", "none")
+
+//INPUT SEGMENT
+
+//input DOM
+//??? Naci bolji nacin razdvajanja radio button-a
+const type = document.querySelector("#type")
+const radios = document.querySelectorAll("input[type=radio]")
+const firstOperand = document.querySelector("#first-operand")
+const secondOperand = document.querySelector("#second-operand")
+const firstLabel = document.querySelector("label[for='first-operand']")
+const secondLabel = document.querySelector("label[for='second-operand'")
+
+const additional = document.querySelector(".custom-dropdown")
+additional.style.setProperty("display", "none")
+
+let bits
+
+//Two way binding for bit input
+let firstOperandInput = "";
+let secondOperandInput = "";
+
+type.addEventListener("change", e => {
+    additional.style.setProperty("display", "block")
+
+    if(type.value.startsWith("multiplier")) {
+        firstLabel.textContent = "Multiplicand"
+        secondLabel.textContent = "Multiplier"     
+        return
+    }
+
+    firstLabel.textContent = "Divident"
+    secondLabel.textContent = "Divisor"
+})
+
+//Changing widths of input fields based on bits needed
+for (const radio of radios) {
+    radio.addEventListener("click", e=> {
+        bits = Number(radio.value)
+        firstOperand.style.setProperty("width", `${bits}ch`)
+        secondOperand.style.setProperty("width", `${bits}ch`)
+        console.log(firstOperand);
+        console.log(secondOperand);
+    })
+}
+
+//Bit inoput validation
+const checkValidation = (input, range) => {
+    const chars = input.split("") 
+
+    if(input == "")
+    return true
+
+    //Checking for 0/1
+    if(!["0", "1"].includes(chars[chars.length-1])) {
+        return false
+    }
+
+    console.log("throguh 0-1");
+    //Checking for the needed length
+    if(chars.length > range) {
+        return false
+    };
+
+    return true
+}
+
+firstOperand.addEventListener("input", e=> {
+    if(checkValidation(firstOperand.value, bits)){
+        firstOperandInput = firstOperand.value
+        return 
+    }
+    firstOperand.value = firstOperandInput
+})
+
+secondOperand.addEventListener("input", e=> {
+    if(checkValidation(secondOperand, bits/2)) {
+        secondOperandInput = secondOperand.value
+        return
+    }
+    secondOperand.value = secondOperandInput
+})
---
+++ MS-Projekat-3-main/pages/adder.js
@@ -0,0 +1,36 @@
+import Multiplicand from './components/multiplicand.js'
+import Multiplier from './components/multiplier.js'    
+import Product from './components/product.js'
+import Control from './components/control.js'
+import ALU from './components/ALU.js'
+import formater from './middleware/formatMiddleware.js'
+
+export default function Adder() {
+
+    const mulLocal = new Multiplicand()
+    const mulpLocal = new Multiplier()
+    const product = new Product(8)
+    const controlLocal = new Control()
+    const formaterLocal = new formater()
+    const ALULocal = new ALU()
+
+    const doStep = () => {
+        
+    }
+}
+
+
+ALULocal.steps.push(mulpLocal.shiftR)
+ALULocal.steps.push(formaterLocal.addBin)
+ALULocal.steps.push(mulLocal.shiftL)
+
+button.addEventListener("click", e=> {
+    const test = ALULocal.stepFunc(product.value, mulLocal.value, mulpLocal.value.slice(-1))
+    
+    if(test) {console.log(test)
+        product.set(test)
+    }
+    app.innerHTML = mulLocal.render() + mulpLocal.render() + product.render() + controlLocal.body + ALULocal.render()
+})
+
+app.innerHTML = mulLocal.render() + mulpLocal.render() + product.render()+ ALULocal.render() + controlLocal.body
